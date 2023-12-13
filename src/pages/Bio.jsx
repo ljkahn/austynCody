dd0e7a1a64e5b7574665309a1043e2968916b539
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-12-13 15:57:23 -0700

    i spelled cody wrong lol

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -4,7 +4,7 @@ function Bio() {
   return (
     <div className="bio-container">
       <h2>
-        Austyn cody works out of Rainbow Demon Tattoo in Denver, CO. Fine line
+        Austyn Cody works out of Rainbow Demon Tattoo in Denver, CO. Fine line
         traditional and japanese.
       </h2>
       <p></p>
