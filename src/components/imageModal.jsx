a6e034f9a2d5811cd844ecb900661ec9a7dfe7b3
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2024-01-05 11:30:34 -0700

    updated modal styling

--- src/components/imageModal.jsx
+++ src/components/imageModal.jsx
@@ -3,13 +3,19 @@ import Modal from "react-bootstrap/Modal";
 
 const ImageModal = ({ show, onHide, imageSrc }) => {
   return (
-    <Modal show={show} onHide={onHide} centered className="modal">
-      <Modal.Body className="text-center p-0">
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Body
+        className="text-center p-0"
+        style={{ backgroundColor: "#1d1d1d" }}
+      >
         <img
           src={imageSrc}
           alt="Large version of the image"
           className="img-fluid"
-          style={{ maxWidth: "100%", maxHeight: "80vh" }}
+          style={{
+            maxWidth: "100%",
+            maxHeight: "80vh",
+          }}
         />
       </Modal.Body>
     </Modal>
