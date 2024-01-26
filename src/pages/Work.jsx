402bb8b2c52f8ae7f5b12674954145aa1cf21e49
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2024-01-26 15:34:02 -0700

    traded bootstrap for material ui masonry layout

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,41 +1,38 @@
 import React, { useEffect, useState } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 import Bio from "../pages/Bio";
 import ImageModal from "../components/imageModal";
 
 import backSnake from "../assets/images/backSnake.jpg";
-import fan from "../assets/images/fan.jpg";
-import spider from "../assets/images/spider.jpg";
-import floralSleeve from "../assets/images/floralSleeve.jpg";
-import hannya from "../assets/images/hannya.jpg";
-import hannya1 from "../assets/images/hannya1.jpg";
-import mumSleeve from "../assets/images/mumSleeve.jpg";
-import mumSleeve2 from "../assets/images/mumSleeve2.jpg";
-import snake from "../assets/images/snake.jpg";
-import snakeCard from "../assets/images/snakeCard.jpg";
-import snakeMum from "../assets/images/snakeMum.jpg";
 import bison from "../assets/images/bison.png";
 import crane from "../assets/images/crane.png";
 import dragon from "../assets/images/dragon.png";
 import face from "../assets/images/face.png";
+import fan from "../assets/images/fan.jpg";
+import floralSleeve from "../assets/images/floralSleeve.jpg";
 import fooDog from "../assets/images/fooDog.png";
 import geoSleeve from "../assets/images/geoSleeve.png";
 import geoSleeve1 from "../assets/images/geoSleeve1.png";
 import Hannya from "../assets/images/hannya.png";
-import hannyaHalf from "../assets/images/hannyaHalf.png";
-import hannyaPaint from "../assets/images/hannyaPaint.png";
+import hannya from "../assets/images/hannya.jpg";
+import hannya1 from "../assets/images/hannya1.jpg";
 import lotus from "../assets/images/lotus.png";
 import mum from "../assets/images/mum.png";
+import mumSleeve from "../assets/images/mumSleeve.jpg";
+import mumSleeve2 from "../assets/images/mumSleeve2.jpg";
 import mumSleevePng from "../assets/images/mumSleeve.png";
 import rose from "../assets/images/rose.png";
 import roseHand from "../assets/images/roseHand.png";
+import snake from "../assets/images/snake.jpg";
 import snakePng from "../assets/images/snake.png";
+import snakeCard from "../assets/images/snakeCard.jpg";
+import snakeMum from "../assets/images/snakeMum.jpg";
 import snakePaint from "../assets/images/snakePaint.png";
+import spider from "../assets/images/spider.jpg";
 import tiger from "../assets/images/tiger.png";
 
-const isContactPage = false;
 function Work() {
   const [showBackToTop, setShowBackToTop] = useState(false);
   const [showModal, setShowModal] = useState(false);
@@ -52,393 +49,85 @@ function Work() {
   };
 
   useEffect(() => {
-    console.log("Effect is running...");
     const handleScroll = () => {
-      console.log("scrolling...");
       if (window.scrollY > 100) {
-        console.log("Setting showBackToTop to true");
         setShowBackToTop(true);
       } else {
-        console.log("Setting showBackToTop to false");
         setShowBackToTop(false);
       }
     };
 
-    // Show or hide the back-to-top button based on scroll position
     window.addEventListener("scroll", handleScroll);
 
-    // Clean up event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []); // Empty dependency array to run the effect only once
+  }, []);
+
+  const itemData = [
+    { img: backSnake, title: "Snake back tattoo" },
+    { img: bison, title: "Bison Back Tattoo" },
+    { img: crane, title: "Crane Tattoo" },
+    { img: dragon, title: "Dragon Tattoo" },
+    { img: face, title: "Blinds" },
+    { img: fan, title: "Chairs" },
+    { img: floralSleeve, title: "Laptop" },
+    { img: fooDog, title: "Doors" },
+    { img: geoSleeve, title: "Coffee" },
+    { img: geoSleeve1, title: "Storage" },
+    { img: hannya, title: "Candle" },
+    { img: Hannya, title: "Coffee table" },
+    { img: hannya1, title: "Coffee table" },
+    { img: lotus, title: "Coffee table" },
+    { img: mum, title: "Coffee table" },
+    { img: mumSleeve, title: "Coffee table" },
+    { img: mumSleeve2, title: "Coffee table" },
+    { img: mumSleevePng, title: "Coffee table" },
+    { img: rose, title: "Coffee table" },
+    { img: roseHand, title: "Coffee table" },
+    { img: snake, title: "Coffee table" },
+    { img: snakePng, title: "Coffee table" },
+    { img: snakeCard, title: "Coffee table" },
+    { img: snakeMum, title: "Coffee table" },
+    { img: snakePaint, title: "Coffee table" },
+    { img: spider, title: "Coffee table" },
+    { img: tiger, title: "Coffee table" },
+  ];
 
   return (
     <>
+      <Bio />
+      <Box
+        sx={{
+          width: "75%",
+          height: "100vh",
+          overflowY: "scroll",
+          margin: "0 auto",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <ImageList variant="masonry" cols={3} gap={8}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+                onClick={() => handleImageClick(item.img)}
+                style={{ width: "100%", height: "100%" }}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
       <div className="work main-container">
-        <Row>
-          <Col md={6}>
-            {/* Include the Bio component here */}
-            <Bio />
-          </Col>
-        </Row>
-        <Row className="text-align-center">
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(backSnake)}
-            >
-              <Card.Img
-                variant="top"
-                src={backSnake}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(fan)}
-            >
-              <Card.Img variant="top" src={fan} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(floralSleeve)}
-            >
-              <Card.Img
-                variant="top"
-                src={floralSleeve}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(hannya1)}
-            >
-              <Card.Img
-                variant="top"
-                src={hannya1}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(mumSleeve)}
-            >
-              <Card.Img
-                variant="top"
-                src={mumSleeve}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(mumSleeve2)}
-            >
-              <Card.Img
-                variant="top"
-                src={mumSleeve2}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(snake)}
-            >
-              <Card.Img variant="top" src={snake} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(snakeCard)}
-            >
-              <Card.Img
-                variant="top"
-                src={snakeCard}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(snakeMum)}
-            >
-              <Card.Img
-                variant="top"
-                src={snakeMum}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(hannya)}
-            >
-              <Card.Img variant="top" src={hannya} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(spider)}
-            >
-              <Card.Img variant="top" src={spider} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(bison)}
-            >
-              <Card.Img variant="top" src={bison} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(crane)}
-            >
-              <Card.Img variant="top" src={crane} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(dragon)}
-            >
-              <Card.Img variant="top" src={dragon} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(face)}
-            >
-              <Card.Img variant="top" src={face} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(fooDog)}
-            >
-              <Card.Img variant="top" src={fooDog} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(geoSleeve)}
-            >
-              <Card.Img
-                variant="top"
-                src={geoSleeve}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(geoSleeve1)}
-            >
-              <Card.Img
-                variant="top"
-                src={geoSleeve1}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(Hannya)}
-            >
-              <Card.Img variant="top" src={Hannya} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(hannyaHalf)}
-            >
-              <Card.Img
-                variant="top"
-                src={hannyaHalf}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(hannyaPaint)}
-            >
-              <Card.Img
-                variant="top"
-                src={hannyaPaint}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(lotus)}
-            >
-              <Card.Img variant="top" src={lotus} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(mum)}
-            >
-              <Card.Img variant="top" src={mum} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(mumSleevePng)}
-            >
-              <Card.Img
-                variant="top"
-                src={mumSleevePng}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(rose)}
-            >
-              <Card.Img variant="top" src={rose} className="work portfolio" />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(roseHand)}
-            >
-              <Card.Img
-                variant="top"
-                src={roseHand}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(snakePng)}
-            >
-              <Card.Img
-                variant="top"
-                src={snakePng}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(snakePaint)}
-            >
-              <Card.Img
-                variant="top"
-                src={snakePaint}
-                className="work portfolio"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Card
-              className="work-card"
-              style={{ backgroundColor: "rgb(20, 20, 20)" }}
-              onClick={() => handleImageClick(tiger)}
-            >
-              <Card.Img variant="top" src={tiger} className="work portfolio" />
-            </Card>
-          </Col>
-        </Row>
         <ImageModal
           show={showModal}
           onHide={handleModalClose}
           imageSrc={selectedImage}
         />
-
         {showBackToTop ? (
           <a
             href="#back-to-top"
